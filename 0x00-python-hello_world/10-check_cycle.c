2733a09fc3f359752cb52886dabfab1096701e77
Abdelnissoul <abdelnissoul205@gmail.com>
2024-01-02 16:50:36 +0000

    hak

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+/**
+ * check_cycle - checks if list is cyclical or not
+ * @list: pointer to the head of list for checking
+ * Return: 0 if there is no cycle, otherwise 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
