f84d0f7c9ced94af1cb81f9d3345daa5e821be81
Abdelnissoul <abdelnissoul205@gmail.com>
2024-01-10 15:22:51 +0000

    correcting in12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return (0)
 
-    roman_val = {'I': 1, 'V': 5, 'X': 10, 'L': 
+    roman_val = {'I': 1, 'V': 5, 'X': 10, 'L':
                  50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     prev_val = 0
