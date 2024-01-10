1d2bff4a6c087ee2f4fcfeeb7f4845d1e93c4a99
Abdelnissoul <abdelnissoul205@gmail.com>
2024-01-10 14:40:37 +0000

    correcting 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    return max(a_dictionary, k=a_dictionary.get)
+    max_value = 0
+    max_key = None
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return max_key
