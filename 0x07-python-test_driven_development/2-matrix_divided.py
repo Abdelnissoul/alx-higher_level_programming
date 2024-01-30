e88e9451333e5f16e4eef9aca2d93e7b4786884a
Abdelnissoul <abdelnissoul205@gmail.com>
2024-01-30 13:49:55 +0000

    kj

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -31,3 +31,4 @@ def matrix_divided(matrix, div):
                 raise TypeError("matrix must be a matrix (list of lists) " +
                                 "of integers/floats")
     return [[round(i / div, 2) for i in row] for row in matrix]
+
