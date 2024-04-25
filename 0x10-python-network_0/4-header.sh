1e7b9acfd438f9cc7250bb412ac3eb1f0ffcd2f0
Abdelnissoul <abdelnissoul205@gmail.com>
2024-04-25 12:47:54 +0100

    solving issuesù

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Takes in a URL as an argument, sends a GET request with a custom header.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -s -X GET -H "X-School-User-Id: 98" "${1}"
