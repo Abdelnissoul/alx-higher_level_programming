888fe5141bc80b83c1ab3a51da8217bf42c0eee8
Abdelnissoul <abdelnissoul205@gmail.com>
2024-04-27 01:24:01 +0100

    status

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     argv = sys.argv
     url = argv[1]
     try:
-        with request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as er:
         print("Error code: {}".format(er.status))
