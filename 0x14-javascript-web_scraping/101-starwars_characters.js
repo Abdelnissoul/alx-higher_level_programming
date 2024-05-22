8e7c46e0d3cf23219ea33780974aafb9c4333f83
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-22 12:17:04 +0100

    trying to sort all starwars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,53 @@
 #!/usr/bin/node
-// priints sall characters of star wars
+// Prints all characters of a Star Wars movie
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./101-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(movieUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch movie details:', response.statusCode);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  const fetchCharacterNames = async () => {
+    for (const characterUrl of characters) {
+      await new Promise((resolve) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            console.error('Error:', error);
+            resolve();
+            return;
+          }
+
+          if (response.statusCode !== 200) {
+            console.error('Failed to fetch character details:', response.statusCode);
+            resolve();
+            return;
+          }
+
+          const character = JSON.parse(body);
+          console.log(character.name);
+          resolve();
+        });
       });
     }
-  }
+  };
+
+  fetchCharacterNames();
 });
