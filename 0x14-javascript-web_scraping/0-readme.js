dcd3d86937e07901da328431e559d39ce72291ec
Abdelnissoul <abdelnissoul205@gmail.com>
2024-05-21 17:16:17 -0700

    Camel case

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // importing and reading the file
 
-const file_s = require('fs');
-file_s.readFile(process.argv[2], 'utf-8',
+const fileSy = require('fs');
+fileSy.readFile(process.argv[2], 'utf-8',
   function (error, data) {
     if (error) {
       console.log(error);
